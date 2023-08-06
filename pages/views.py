8748147aea06fca20095bbb8764f7a97bd351a4c
Tushar-Hegde <128315293+Tushar-Hegde@users.noreply.github.com>
2023-08-06 11:01:42 +0530

    Add files via upload

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse 
 from forums.models import Forum,Events
+from datetime import datetime
+from django.db.models import Q
+
 # Create your views here.
 
 def home(request):
     if request.user.is_authenticated:
         forums = request.user.forums.all()
-        events = Events.objects.filter(forum__in=forums)
+        events = Events.objects.filter(forum__in=forums).order_by('date').filter(Q(date__gte=datetime.now()))
         context={'forums':forums,'events':events,}
         return render(request,'home.html',context)
     else:
-        return render(request,'login.html')
+        return redirect(reverse('users:login'))
